fd4b1e98a3283601cd17cb4a85824d4c88693b14
Artem Riabykh <riabykhartem@yandex.ru>
2023-10-01 22:20:53 -0500

    firts attempt

--- routes/cards.js
+++ routes/cards.js
@@ -11,7 +11,7 @@ router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.url().required().min(2).max(30),
+    link: Joi.string().required().min(2).max(30),
   }),
 }), authorization, createCard);
 
